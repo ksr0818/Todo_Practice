97895a0f1d97d982be4bc75a77659c23c0bd2ebf
ksr0818 <ksr940818@gmail.com>
2023-04-13 01:53:34 +0900

    TodoController commit

---
+++ src/test/java/com/example/Todo_App_Application/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Todo_App_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
